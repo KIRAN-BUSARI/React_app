45a349f62a82ece847165d24982b66d5b49cef86
KIRAN-BUSARI <kiranbusari2208@gmail.com>
2023-08-10 17:16:30 +0530

    Added files with some changes in app, logo, Added Alert.js,About.js file and also modified the textform, navbar,

--- src/App.js
+++ src/App.js
@@ -1,10 +1,56 @@
 import Navbar from './components/Navbar';
 import './App.css';
+import TextForm from './components/Textform';
+import { useState } from 'react';
+import Alerts from './components/Alert';
+
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+import About from './components/About';
+
 
 function App() {
+  const [mode, setMode] = useState('light');
+  const [alert, setAlert] = useState(null);
+
+
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type
+    })
+    setTimeout(() => {
+      setAlert(null)
+    }, 1000);
+  }
+
+  const toggleMode = () => {
+    if (mode === 'light') {
+      setMode('dark');
+      document.body.style.backgroundColor = '#000000';
+      showAlert("Dark Mode has been Enabled!", "success");
+    }
+    else {
+      setMode('light');
+      document.body.style.backgroundColor = 'white';
+      showAlert("Light Mode has been Enabled!", "success");
+    }
+  }
   return (
     <>
-      <Navbar />
+      <BrowserRouter>
+        <Navbar heading="Knowly" mode={mode} toggleMode={toggleMode} />
+        <Alerts alert={alert} />
+        <div className="container my-3">
+          <Routes>
+            <Route path='/about' element={<About />} />
+            <Route path='/' element={<TextForm heading="Enter your text here 😊" mode={mode} showAlert={showAlert} />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
     </>
   );
 }
---
+++ src/components/Textform.js
@@ -0,0 +1,59 @@
+import React, { useState } from 'react'
+
+export default function TextForm(props) {
+
+    const handleOnClickUpper = () => {
+        let newText = text.toUpperCase();
+        setText(newText)
+        props.showAlert("Converted to uppercase!", "success");
+    }
+
+    const handleOnChange = (event) => {
+        setText(event.target.value);
+    }
+
+    const handleOnClickLower = () => {
+        let newText = text.toLowerCase();
+        setText(newText);
+        props.showAlert("Converted to lowercase!", "success");
+    }
+
+    const handleClearText = () => {
+        let newText = '';
+        setText(newText);
+        props.showAlert("Text Cleared!", "success");
+    }
+
+    const handleCopy = () => {
+        var text = document.getElementById("my-box");
+        text.select();
+        navigator.clipboard.writeText(text.value);
+        props.showAlert("Copied Text!", "success");
+    }
+
+    const [text, setText] = useState("");
+
+    return (
+        <>
+            <div className="mb-3" style={{ color: props.mode === 'dark' ? 'white' : 'black' }}>
+                <h1>{props.heading}</h1>
+                <textarea
+                    style={{ backgroundColor: props.mode === 'dark' ? 'black' : 'white', color: props.mode === 'dark' ? 'white' : 'black' }}
+                    onChange={handleOnChange}
+                    value={text}
+                    className="form-control"
+                    id="my-box"
+                    rows={3}
+                />
+                <button className="btn btn-primary my-3 mx-2" onClick={handleOnClickUpper} >Convert to UPPERCASE</button>
+                <button className="btn btn-primary my-3 mx-2" onClick={handleOnClickLower} >CONVERT TO lowercase</button>
+                <button className="btn btn-primary my-3 mx-2" onClick={handleClearText} >Clear Text</button>
+                <button className="btn btn-primary my-3 mx-2" onClick={handleCopy} >Copy Text</button>
+                <div>
+                    <p>{"Number of words : " + text.split(" ").filter((element) => {return element != 0 }).length}</p>
+                    <p>{"Number of characters : " + text.length}</p>
+                </div>
+            </div>
+        </>
+    )
+}
